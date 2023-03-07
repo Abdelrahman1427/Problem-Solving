3ba1c9191a2ec38b1c4efd1d8618631be332db7c
abdelrahman1427 <abdelrahman.abdalla@stemredsea.moe.edu.eg>
2023-03-07 23:18:20 +0200

    using vector & count method

---
+++ A/A_I_Wanna_Be_the_Guy.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int p, level;
+    set<int> levelsX, levelsY;
+
+    cin >> p;
+    for (int i = 0; i < p; i++) {
+        cin >> level;
+        levelsX.insert(level);
+    }
+
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        cin >> level;
+        levelsY.insert(level);
+    }
+
+    bool canPass = true;
+    for (int i = 1; i <= n; i++) {
+        if (!levelsX.count(i) && !levelsY.count(i)) {
+            canPass = false;
+            break;
+        }
+    }
+
+    if (canPass) {
+        cout << "I become the guy.";
+    } else {
+        cout << "Oh, my keyboard!";
+    }
+
+    return 0;
+}
