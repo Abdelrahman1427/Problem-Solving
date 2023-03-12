242ab62cb2bef2d563b901763e18efb30e8ef3f5
abdelrahman1427 <abdelrahman.abdalla@stemredsea.moe.edu.eg>
2023-03-13 00:30:49 +0200

    Accepted

--- A/A_Brain_s_Photos.cpp
+++ A/A_Brain_s_Photos.cpp
@@ -1,27 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    char c[10] = {'C', 'M', 'Y', 'W', 'G', 'B'};
+    char c[6] = {'C', 'M', 'Y', 'W', 'G', 'B'};
     int a, b;
     cin >> a >> b;
 
     char cc[a][b];
     bool flage = true;
-    for (int i = 0; i < b; i++)
+    int color = 0, bw = 0;
+
+    for (int i = 0; i < a; i++)
     {
         for (int j = 0; j < b; j++)
         {
             cin >> cc[i][j];
             if (cc[i][j] == 'C' || cc[i][j] == 'M' || cc[i][j] == 'Y')
-                flage = false;
+            {
+                color++;
+            }
         }
     }
 
-    if (flage)
-        cout << "Black&White";
-    else
-
+    if (color >= 1)  // elmshkla kant hana 
+    {
         cout << "#Color";
+    }
+    else
+    {
+        cout << "#Black&White";
+    }
+
     return 0;
 }
